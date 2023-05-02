25a0f1f6956a6f97a3e7f1103851207229123e51
DakshSutariya <130463070+DakshSutariya@users.noreply.github.com>
2023-05-02 17:58:49 +0530

    Add files via upload

---
+++ parttens for loop 6 .c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	int r,c;
+	
+	for(r=5;r>=1;r--)
+	{
+		for(c=r;c<=;c++)
+		{
+			printf("%d",r);
+		}
+		printf("\n");
+	}
+}
+/*
+5
+4 4
+3 3 3
+2 2 2 2
+1 1 1 1 1
+*/
